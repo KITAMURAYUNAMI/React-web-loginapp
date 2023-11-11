88b56cbabffc6a78742fed572c1201e5506134ee
KITAMURAYUNAMI <149940574+KITAMURAYUNAMI@users.noreply.github.com>
2023-11-11 13:57:24 +0900

    説明を加えました

--- src/hooks/userLoginUser.ts
+++ src/hooks/userLoginUser.ts
@@ -6,3 +6,5 @@ import {
 
 export const useLoginUser = (): LoginUserContextType =>
   useContext(LoginUserContext);
+//本来は各ページでuseContextをしてそれに型指定をするという流れをしなければならないが、ここでその二つの作業を一回にまとめている
+//useContextとはグローバルなstateを使いますよという宣言
